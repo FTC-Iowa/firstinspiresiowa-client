3c8394e25cedcafa6b5bdf28b1f75df0dbd0657e
Jeramie Vens <Jeramie.Vens@gmail.com>
2017-10-12 14:45:44 -0500

    Parse match details file

--- src/org/firstinspiresiowa/client/Main.java
+++ src/org/firstinspiresiowa/client/Main.java
@@ -10,8 +10,9 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.InputStreamReader;
 
-import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import javax.net.ssl.HttpsURLConnection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
@@ -24,42 +25,65 @@ public class Main {
     private final String USER_AGENT = "Mozilla/5.0";
 
 
+    
+    Element getTable(File f) throws Exception{
+        Document doc = Jsoup.parse(f, "UTF-8", "");
+        Elements tables = doc.getElementsByTag("table");
+        if (tables.isEmpty()) {
+            System.err.println("Could not find table in HTML document");
+            throw new Exception();
+        }
+        return tables.first();
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
         Main http = new Main();
         
-        File input = new File("/home/vens/Projects/ftc-scoring-1-0/reports/MatchResultsDetails_meet_name.html");
-        Document doc = Jsoup.parse(input, "UTF-8", "");
-        Elements tables = doc.getElementsByTag("table");
+        File matchDetailsFile = new File("MatchResultsDetails_North_Super_Regional_Kindig.html");
+        File teamInfoFile = new File("TeamInfo_North_Super_Regional_Kindig.html");
+        File rankingsFile = new File("Rankings_North_Super_Regional_Kindig.html");
         
-        if(tables.isEmpty()) {
-            System.err.println("Could not find table in HTML document");
-            return;
+        Element matchDetailsTable = http.getTable(matchDetailsFile);
+        Element teamInfoTable = http.getTable(teamInfoFile);
+        Element rankingsTable = http.getTable(rankingsFile);
+        
+        Elements matchDetailsRows = matchDetailsTable.getElementsByTag("tr");
+        ArrayList<Match> matches = new ArrayList<Match>();
+        for(int i = 2; i < matchDetailsRows.size(); i++) { // start at row 2 to get past the two headers
+            Element row = matchDetailsRows.get(i);
+            matches.add(new Match(row, i - 1, "kindig"));
         }
         
-        Element table = tables.first();
-        Elements rows = table.getElementsByTag("tr");
-        System.out.println(rows.size());
-        Element row = rows.get(2);
-        Match m = new Match(row, 1, "davenport");
+        Elements teamInfoRows = teamInfoTable.getElementsByTag("tr");
+        ArrayList<Team> teams = new ArrayList<Team>();
+        for(int i = 1; i< teamInfoRows.size(); i++) {
+            Element row = teamInfoRows.get(i);
+            teams.add(new Team(row));
+        }
         
+        for (Match m: matches) {
+            http.sendPost(m.BuildJson());
+        }
         
-        http.sendGet();
+        for (Team t: teams) {
+            http.sendPost(t.BuildJson());
+        }
         
-        http.sendPost(m);
+        //http.sendGet();
     }
     
     
     // HTTP GET request
 	private void sendGet() throws Exception {
 
-		//String url = "https://firstinspiresiowa.firebaseapp.com/event/davenport";
-                String url = "http://localhost:5000/event/davenport";
+		String url = "https://firstinspiresiowa.firebaseapp.com/event/davenport";
+                //String url = "http://localhost:5000/event/davenport";
 		URL obj = new URL(url);
-		//HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+		//HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
 		// optional default is GET
 		con.setRequestMethod("GET");
@@ -87,13 +111,13 @@ public class Main {
 	}
 
 	// HTTP POST request
-	private void sendPost(Match m) throws Exception {
+	private void sendPost(String data) throws Exception {
 
-		//String url = "https://firstinspiresiowa.firebaseapp.com/event/davenport";
-                String url = "http://localhost:5000/event/davenport";
+		String url = "https://firstinspiresiowa.firebaseapp.com/event/davenport";
+                //String url = "http://localhost:5000/event/davenport";
 		URL obj = new URL(url);
-		//HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+		//HttpURLConnection con = (HttpURLConnection) obj.openConnection();
                 
 		//add reuqest header
 		con.setRequestMethod("POST");
@@ -101,12 +125,7 @@ public class Main {
 		con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
                 con.setRequestProperty("Content-Type", "application/json");
 
-                //Match m = new Match("");
-                
-		//String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
-
-                //String data = "{\"name\":\"jeramie\",\"num\":\"2\"}";
-                String data = m.BuildJson();
+                //String data = m.BuildJson();
                 
 		// Send post request
 		con.setDoOutput(true);
@@ -116,8 +135,8 @@ public class Main {
 		wr.close();
 
 		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + data);
+		//System.out.println("\nSending 'POST' request to URL : " + url);
+		//System.out.println("Post parameters : " + data);
 		System.out.println("Response Code : " + responseCode);
 
 		BufferedReader in = new BufferedReader(
